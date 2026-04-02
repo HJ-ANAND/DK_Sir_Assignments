15006930aa39b1eb888b4875cd27bc2dda6cf3db
HJ-ANAND <anandharshjeet054@gmail.com>
2026-04-02 20:04:37 +0530

    react revision class

---
+++ 2_manual_component/src/App.jsx
@@ -0,0 +1,12 @@
+import Card1 from "./components/Card1";
+import Card2 from "./components/Card2";
+
+function App() {
+  return (
+    <>
+      <Card1 name="Mouse" price="1230" />
+    </>
+  );
+}
+
+export default App;
---
+++ 2_manual_component/src/components/Card1.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Card1({ name, price }) {
+  return (
+    <div>
+      Card1
+      <h1>{name}</h1>
+      <h1>{price}</h1>
+    </div>
+  );
+}
+
+export default Card1;
